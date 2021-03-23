dee224467f2e6fba621f7daf7ea2b1b69368c376
Carlos Agullo <software.development.2@olympicchannel.com>
2021-03-23 16:51:33 +0100

    refactoring code

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,40 @@
 import cheerio from "cheerio";
 import request from "tinyreq";
 
+interface GamesType {
+  year: number;
+  location: string;
+  url: string;
+};
+
 const olympediaUrl = "https://www.olympedia.org/editions";
 const tableGameSelector = "table.table.table-striped:first-of-type>tbody";
-const getGamesUrls = async () => {
+
+const MAX_GAME_YEAR = 2020;
+const BASE_TEN = 10;
+
+const getGamesUrls = async (): Promise<GamesType[]> => {
+  let games: GamesType[] = [];
   const data = await request(olympediaUrl);
   const $ = cheerio.load(data);
   const gamesColumn = $(
     `${tableGameSelector} tr td:nth-child(3) a`
   );
     gamesColumn.each((i, elem) => {
-      const gameYear = $(`${tableGameSelector} tr:nth-child(${i + 1}) td:nth-child(2)`).text();
-      console.log(i + 1, $(elem).text(), gameYear, '\nurl:', olympediaUrl + $(elem).attr('href'), '\n');
-  });
+      const gameYear = parseInt($(`${tableGameSelector} tr:nth-child(${i + 1}) td:nth-child(2)`).text(), BASE_TEN);
+      const location = $(elem).text();
+      const url = olympediaUrl + $(elem).attr('href');
+      if (gameYear < MAX_GAME_YEAR) {
+        games.push({
+          year: gameYear,
+          location: location,
+          url: url
+        })
+      }
+    });
+  return games;
 };
 (async () => {
-  await getGamesUrls();
+  const results = await getGamesUrls();
+  console.log(results);
 })();
